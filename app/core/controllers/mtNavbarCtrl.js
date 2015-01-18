90b81a826d20aa7f6a705a0b9c3c8612524a172d
Matt <Matt@Deep-Thought.local>
2015-01-18 04:36:06 -0700

    Split into mt-navbar directive

--- app/core/controllers/mtNavbarCtrl.js
+++ app/core/controllers/mtNavbarCtrl.js
@@ -1,3 +1,11 @@
 app.controller('mtNavbarCtrl', ['$scope', function ($scope) {
 
 }]);
+
+app.directive('mtNavbar', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/app/core/partials/navbar.html',
+    controller: 'mtNavbarCtrl'
+  };
+});
