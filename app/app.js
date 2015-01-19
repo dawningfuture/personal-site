6e32626e28a5b79ea515cddeea4214585730976e
Matt <Matt@Deep-Thought.local>
2015-01-18 21:10:12 -0700

    Implement Route Titles and adjust css for fullscreen resume

--- app/app.js
+++ app/app.js
@@ -1,21 +1,35 @@
 var app = angular.module('mtPersonal', ['ngRoute']);
 
 app.config(['$routeProvider',
-    function($routeProvider) {
+    function ($routeProvider) {
         $routeProvider.
-            when('/home', {
-                templateUrl: '/app/core/partials/under-construction.html',
-                controller: 'mtUnderConstructionCtrl'
-            }).
-            when('/about', {
-                templateUrl: '/app/core/partials/about.html',
-                controller: 'mtAboutCtrl'
-            }).
-            when('/resume', {
-                templateUrl: '/app/core/partials/resume.html',
-                controller: 'mtResumeCtrl'
-            }).
-            otherwise({
-                redirectTo: '/home'
-            });
+        when('/home', {
+            title: 'Matthew Thompson\'s Personal Web Presence',
+            templateUrl: '/app/core/partials/under-construction.html',
+            controller: 'mtUnderConstructionCtrl'
+        }).
+        when('/about', {
+            title: 'About Matthew Thompson',
+            templateUrl: '/app/core/partials/about.html',
+            controller: 'mtAboutCtrl'
+        }).
+        when('/resume', {
+            title: 'Resume',
+            templateUrl: '/app/core/partials/resume.html',
+            controller: 'mtResumeCtrl'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
     }]);
+app.run(['$rootScope', '$route',
+    function ($rootScope, $route) {
+
+        /* Updates the page title on route change */
+
+        $rootScope.$on('$routeChangeSuccess', function (newVal, oldVal) {
+            if (oldVal !== newVal) {
+                document.title = $route.current.title;
+            }
+        });
+}]);
