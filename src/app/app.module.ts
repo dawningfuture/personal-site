229d88669a5d3af46732b04adac7b599abc2fe7b
Matthew Thompson <dawningfuture@gmail.com>
2021-07-31 14:12:43 -0600

    feat: #11 close sidenav on navigation
    ISSUES CLOSED: 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { RouterEffects } from './state/effects/router.effects';
 import { reducers } from './state/reducers';
 
 export const appImports = [
   BrowserAnimationsModule,
+  AppRoutingModule,
   StoreModule.forRoot(reducers, {
     runtimeChecks: {
       strictActionTypeUniqueness: true,
     },
   }),
-  EffectsModule.forRoot([]),
+  EffectsModule.forRoot([RouterEffects]),
+  StoreRouterConnectingModule.forRoot(),
 ];
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     LayoutModule,
     ...appImports,
     !environment.production ? StoreDevtoolsModule.instrument() : [],
--- src/app/layout/sidenav/sidenav-link/sidenav-link.component.ts
+++ src/app/layout/sidenav/sidenav-link/sidenav-link.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SidenavLinkComponent implements OnInit {
   @Input() label = '';
+  @Input() path?: string;
 
   constructor() {}
 
