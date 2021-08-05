c909b6e8e49126d9a21c0dd906d0d816ba9672db
Matthew Thompson <dawningfuture@gmail.com>
2021-08-04 23:18:43 -0600

    feat: #3 add staging deployment
    ISSUES CLOSED: 3

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+import packageJson from '../../package.json';
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
+  production: true,
+  version: packageJson.version,
+};
