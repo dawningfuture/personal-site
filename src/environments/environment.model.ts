e26fc673693a2d37d018a3dd17e63fe982b63721
Matthew Thompson <dawningfuture@gmail.com>
2021-08-07 07:58:54 -0600

    fix: #48 redirect to environment origin

--- src/environments/environment.model.ts
+++ src/environments/environment.model.ts
@@ -1,4 +1,18 @@
 export interface Environment {
-    production: boolean;
-    version: string;
+  /**
+   * Whether the configuation should be compiled for production
+   */
+  production: boolean;
+
+  /**
+   * App version
+   */
+  version: string;
+
+  /**
+   * Used in production only
+   *
+   * Origin from which the app must be served
+   */
+  origin: string;
 }
