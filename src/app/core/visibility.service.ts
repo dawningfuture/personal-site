4507e1dbb2fa30ba564c445f53ab87298072becd
Matthew Thompson <dawningfuture@gmail.com>
2021-09-08 05:07:26 -0600

    docs: #74 add comments to core services

--- src/app/core/visibility.service.ts
+++ src/app/core/visibility.service.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Subject } from 'rxjs';
 import * as Visibility from 'visibilityjs';
 
+// NOTE: I got the keys for the visibility states for `visibilityjs` here:
+// https://github.com/ai/visibilityjs#states
+
+export enum VisibilityStates {
+  HIDDEN = 'hidden',
+  VISIBLE = 'visible',
+}
 @Injectable({
   providedIn: 'root',
 })
 export class VisibilityService {
-  private hidden = new Subject<void>();
-  readonly hidden$ = this.hidden.asObservable();
+  // NOTE: When I use services for messaging, I make the underlying `Subject`
+  // private, and make its emissions accessible using a different public variable
+  // and the `asObservable()` method. This ensures that consumers of the service
+  // can't override the visbility detection done by `visibilityjs` from outside the service.
 
-  private visibile = new Subject<void>();
-  readonly visibile$ = this.visibile.asObservable();
+  private visibilityChange = new Subject<VisibilityStates>();
+  readonly visibilityChange$ = this.visibilityChange.asObservable();
 
-  constructor() {
-    Visibility.change((event, state) => {
-      if (state === 'hidden') {
-        this.hidden.next();
-      }
-
-      if (state === 'visible') {
-        this.visibile.next();
-      }
-    });
+  constructor(private zone: NgZone) {
+    Visibility.change((_, state) =>
+      this.zone.run(() => this.visibilityChange.next(state as VisibilityStates))
+    );
   }
 }
