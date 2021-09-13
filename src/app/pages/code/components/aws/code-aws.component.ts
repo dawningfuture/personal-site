de67680ed98ba3a85bf906d23d2010e5009ae89c
Matthew Thompson <dawningfuture@gmail.com>
2021-09-13 03:56:50 -0600

    WIP: #74 cleanup for PR

--- src/app/pages/code/components/aws/code-aws.component.ts
+++ src/app/pages/code/components/aws/code-aws.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ps-code-aws',
   templateUrl: './code-aws.component.html',
-  styleUrls: ['./code-aws.component.scss']
+  styleUrls: ['./code-aws.component.scss'],
 })
-export class CodeAwsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class CodeAwsComponent {}
